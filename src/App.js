eb4f3df3b555d680594decb0f5f5985c4da0b42c
yiladien@hotmail.com <yiladien@hotmail.com>
2023-05-30 00:27:16 -0500

    share page and pwa install

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+// import React, { useState } from "react";
 import React, { useState } from "react";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Media from "./pages/Media";
+import Share from "./pages/Share";
 // import Example from "./pages/Example";
 
 function App() {
-  const [viewHeight, setViewHeight] = useState(
-    navigator.userAgent.indexOf("iPhone") === -1 ? 110 : 110
-  );
+  // const [viewHeight, setViewHeight] = useState(
+  //   navigator.userAgent.indexOf("iPhone") === -1 ? 110 : 110
+  // );
 
   return (
-    <div className="App" style={{ height: `${viewHeight}vh` }}>
+    // <div className="App" style={{ height: `${viewHeight}vh` }}>
+    <div className="App">
       <div className="text-white">
-        <Home viewHeight={viewHeight} />
+        {/* <Home viewHeight={viewHeight} />
         <About viewHeight={viewHeight} />
         <Contact viewHeight={viewHeight} />
         <Media viewHeight={viewHeight} />
+        <Share viewHeight={viewHeight} /> */}
+
+        <Home />
+        <About />
+        <Contact />
+        <Media />
+        <Share />
         {/* <Example /> */}
       </div>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 //bootstrap
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 
 import {
   Envelope,
