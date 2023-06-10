17c73653d91b02739e5c4acc9db5812535c14785
yiladien@hotmail.com <yiladien@hotmail.com>
2023-06-10 00:23:47 -0500

    image media queries

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -11,31 +11,27 @@ import qrcode from "../assets/images/qrcode/qrcode-url-www.madisonolguin.com.png
 const Share = () => {
   const [showModal, setShowModal] = useState(false);
 
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <section className="scroll-container">
-      <div className="page-container d-flex flex-column align-content-center">
+      <div className="page-container d-flex flex-column align-content-center pb-2">
         <Row>
           <Col className="d-flex justify-content-center">
             <h1 className="mb-1">Share</h1>
           </Col>
         </Row>
-        <Row className="qrcode-box flex-fill align-content-center justify-content-center pb-5">
-          {/* <Col xs={8} md={4} xl={2}> */}
-
-          <Col
-            xs={8}
-            md={4}
-            xl={2}
-            className="d-flex align-content-center justify-content-center pb-3"
-          >
+        <div className="qrcode-box d-flex flex-column align-content-center justify-content-center w-100 h-100 flex-grow-0 pb-5 ps-5 pe-5">
+          <div className="d-inline-flex align-content-center justify-content-center mw-100 mh-100">
             <Image
-              className="click-pointer"
-              fluid
+              className="click-pointer mh-100"
               src={qrcode}
-              onClick={() => setShowModal(!showModal)}
+              onClick={handleModal}
             />
-          </Col>
-        </Row>
+          </div>
+        </div>
       </div>
       <Modal
         className="qrcode-modal"
@@ -46,7 +42,7 @@ const Share = () => {
       >
         <Modal.Body
           className="p-0 text-center bg-dark bg-opacity-10 d-flex align-items-center justify-content-center"
-          onClick={() => setShowModal(!showModal)}
+          onClick={handleModal}
         >
           <Image className="max-vh-100 rounded-4" src={qrcode} fluid />
         </Modal.Body>
