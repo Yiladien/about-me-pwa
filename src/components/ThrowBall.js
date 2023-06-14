5e053787c50aa5456477f25e961378af8f815c63
yiladien@hotmail.com <yiladien@hotmail.com>
2023-06-13 23:15:52 -0500

    home animation

--- src/components/ThrowBall.js
+++ src/components/ThrowBall.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-//bootstrap
-import Image from "react-bootstrap/Image";
-
 import {
   throwball50Vert,
   throwball25Vert,
@@ -41,9 +38,15 @@ const ThrowBall = () => {
   // };
 
   return (
-    <div id="full-container">
-      <div id="image-container">
-        <picture id="throw-no-ball-transparent" className="drop-shadow">
+    <div
+      id="full-container"
+      className="d-flex align-items-center justify-content-center w-100 h-100 flex-column position-relative"
+    >
+      <div
+        id="image-container"
+        className="d-flex-inline mw-100 mh-100 position-relative overflow-hidden pb-5"
+      >
+        <picture>
           <source
             srcSet={throwball50Vert}
             media="only screen and (min-width: 679px)"
@@ -54,10 +57,30 @@ const ThrowBall = () => {
             media="only screen and (min-width: 272px)"
             type="image/png"
           />
-          <img
+          <motion.img
+            initial={{
+              opacity: 0,
+              scale: 0,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              opacity: {
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              scale: {
+                duration: 1.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+            }}
             id="throw-no-ball-transparent"
+            className="img-fluid opacity-0 mw-100 mh-100"
             src={throwball10Vert}
-            className="img-fluid"
             alt="madison-olguin"
           />
         </picture>
@@ -67,24 +90,43 @@ const ThrowBall = () => {
           alt="madison-olguin"
         /> */}
 
-        <picture id="throw-no-ball" className="drop-shadow">
+        <picture>
           <source
             srcSet={throwballNoBall50Vert}
             media="only screen and (min-width: 679px)"
             type="image/png"
-            className="img-fluid"
             alt="madison-olguin"
           />
           <source
             srcSet={throwballNoBall25Vert}
             media="only screen and (min-width: 272px)"
             type="image/png"
-            className="img-fluid"
             alt="madison-olguin"
           />
-          <img
+          <motion.img
+            initial={{
+              opacity: 0,
+              scale: 0,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              opacity: {
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              scale: {
+                duration: 1.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+            }}
+            id="throw-no-ball"
+            className="img-fluid drop-shadow mw-100 mh-100 position-absolute top-0 start-0"
             src={throwballNoBall10Vert}
-            className="img-fluid"
             alt="madison-olguin"
           />
         </picture>
@@ -95,24 +137,64 @@ const ThrowBall = () => {
           className="drop-shadow"
         /> */}
 
-        <picture id="ball" className="drop-shadow">
+        <picture>
           <source
             srcSet={throwballBall50Vert}
             media="only screen and (min-width: 679px)"
             type="image/png"
-            className="img-fluid"
             alt="madison-olguin"
           />
           <source
             srcSet={throwballBall25Vert}
             media="only screen and (min-width: 272px)"
             type="image/png"
-            className="img-fluid"
             alt="madison-olguin"
           />
-          <img
+
+          <motion.img
+            initial={{
+              opacity: 0,
+              scale: 0,
+              rotate: 0,
+              originX: "70.3%",
+              originY: "70.2%",
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+              rotate: 360,
+              originX: "26.3%",
+              originY: "22.2%",
+            }}
+            transition={{
+              originX: {
+                duration: 1.2,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              originY: {
+                duration: 1.2,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              opacity: {
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              scale: {
+                duration: 1.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              rotate: {
+                repeat: Infinity,
+                duration: 20,
+              },
+            }}
+            id="ball"
+            className="img-fluid drop-shadow mw-100 mh-100 position-absolute top-0 start-0"
             src={throwballBall10Vert}
-            className="img-fluid"
             alt="madison-olguin"
           />
         </picture>
